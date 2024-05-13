b12534fe1515601b79400e40b5d6770c0d8eeb8f
vlad <vladislav.tishchenko@mail.ru>
2024-05-13 23:14:35 +0300

    V1.7

--- omega/shopapp/urls.py
+++ omega/shopapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('result/', views.result, name='result'),
+    #path('result/', views.result, name='result'),
     path('about', views.about)
 ]
--- omega/shopapp/views.py
+++ omega/shopapp/views.py
@@ -3,15 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'shopapp/home.html')
-
-
-def result(request):
-    num1 = int(request.GET.get('number1'))
-    num2 = int(request.GET.get('number2'))
+    ans = None
+    if request.GET.get('number1') and request.GET.get('number2'):
+        num1 = int(request.GET.get('number1'))
+        num2 = int(request.GET.get('number2'))
     if request.GET.get('add') == "":
         ans = num1 + num2
-    return render(request, 'shopapp/result.html', {'ans': ans})
+    return render(request, 'shopapp/home.html', {'ans': ans})
+
+
+#def result(request):
+#    num1 = int(request.GET.get('number1'))
+#    num2 = int(request.GET.get('number2'))
+#    if request.GET.get('add') == "":
+#        ans = num1 + num2
+#    return render(request, 'shopapp/result.html', {'ans': ans})
 
 def about(request):
     return render(request, 'shopapp/about.html')
