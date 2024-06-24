e68fa308de5a56e5782ac6cde26788a9a06d86c2
Di <93031747+Dionaat@users.noreply.github.com>
2024-06-25 01:30:49 +0300

    news done!!

--- src/components/pyk/News.js
+++ src/components/pyk/News.js
@@ -1,37 +1,10 @@
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'; // Для работы с HTTP запросами
-
-const Card = ({ cardId }) => {
-  const [cardData, setCardData] = useState(null);
-
-  useEffect(() => {
-    // Функция для получения данных с бэкенда
-    const fetchCardData = async () => {
-      try {
-        const response = await axios.get(`https://api.omega-centre.ru/api/v1/new/${cardId}`);
-        setCardData(response.data); // Устанавливаем данные карточки из ответа
-      } catch (error) {
-        console.error('Error fetching card data:', error);
-      }
-    };
-
-    fetchCardData(); // Вызываем функцию загрузки данных при монтировании компонента
-  }, [cardId]); // Монтируем компонент при изменении cardId
-
-  if (!cardData) {
-    return <div>Loading...</div>; // Пока данные загружаются, показываем сообщение о загрузке
-  }
-
+const Card = ({ cardData }) => {
   return (
     <div className="card">
       <h2>{cardData.title}</h2>
-      <p>{cardData.description}</p>
+      <p>{cardData.content}</p>
     </div>
   );
 };
 
 export default Card;
-
-
-
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,17 +1,38 @@
-import Layout from "../components/Layout";
-//import news from "../css/news";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Layout from '../components/Layout';
+import Card from '../components/pyk/News'; // Убедитесь, что этот путь правильный
 
+const News = () => {
+  const [cardsData, setCardsData] = useState(null);
 
-function News () {
-    return (
-        <Layout>
-                <h1>Новости</h1>
-                <div>
-                    запрос на бэк
-                </div>
+  const fetchCardData = async () => {
+    try {
+      const response = await axios.get("https://api.omega-centre.ru/api/v1/new/");
+      setCardsData(response.data.posts); // Устанавливаем данные карточки из ответа
+    } catch (error) {
+      console.error('Error fetching card data:', error);
+    }
+  };
 
-        </Layout>
-    )
-}
+  useEffect(() => {
+    fetchCardData();
+  }, []); // Зависимость пустая, чтобы функция вызывалась только один раз при монтировании
+
+  if (!cardsData) {
+    return <div>Loading...</div>; // Пока данные загружаются, показываем сообщение о загрузке
+  }
+
+  return (
+    <Layout>
+      <h1>Новости</h1>
+      <div>
+        {cardsData.map((card) => (
+          <Card key={card.id} cardData={card} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default News;
